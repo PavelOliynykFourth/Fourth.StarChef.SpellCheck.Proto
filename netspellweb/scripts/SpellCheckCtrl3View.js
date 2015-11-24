d51d454821adb99946a14b574a24d432212cc7c6
unknown <poliynyk@LAT-7250-020.fourth.local>
2015-11-24 16:52:29 +0000

    Fix

--- netspellweb/scripts/SpellCheckCtrl3View.js
+++ netspellweb/scripts/SpellCheckCtrl3View.js
@@ -1,13 +1,2 @@
-﻿function AppViewModel() {
-    this.instantaneousValue = ko.observable();
-    this.throttledValue = ko.computed(this.instantaneousValue)
-                            .extend({ throttle: 400 });
-
-    // Keep a log of the throttled values
-    this.loggedValues = ko.observableArray([]);
-    this.throttledValue.subscribe(function (val) {
-        if (val !== '')
-            this.loggedValues.push(val);
-    }, this);
-}
+﻿
 
